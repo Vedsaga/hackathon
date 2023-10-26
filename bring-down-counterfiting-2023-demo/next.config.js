ef5d6902c8ecc444279bb788f55077b9802f992a
harshit kumar <harshit.kumar@esamudaay.com>
2023-10-26 14:35:45 +0530

    added images domain in next-config

--- bring-down-counterfiting-2023-demo/next.config.js
+++ bring-down-counterfiting-2023-demo/next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["tsdr.uspto.gov", "images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tsdr.uspto.gov",
+        port: "",
+        pathname: "/img/**/large",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/photo-**-**-?auto=format&fit=crop&w=300&q=80",
+      },
+    ],
+  },
+};
