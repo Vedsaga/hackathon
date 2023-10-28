63d29946f825611b79e88d5c7247783474afe217
harshit kumar <harshit.kumar@esamudaay.com>
2023-10-28 20:38:01 +0530

    change the title

--- bring-down-counterfiting-2023-demo/app/page.tsx
+++ bring-down-counterfiting-2023-demo/app/page.tsx
@@ -8,19 +8,21 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col min-h-screen items-center justify-between p-32">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
-
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <div className="group text-center">
-          <h1 className="mb-3 text-3xl font-semibold">
-            Bring Down Counterfeiting
-          </h1>
-          <h4 className="mb-3 text-2xl font-black">2023 Demo</h4>
+        <div className="group text-center space-y-4">
+          <h1 className="text-8xl font-extrabold  leading-tight">Team 3P2L</h1>
+          <h4 className="text-4xl leading-normal mt-3 font-semibold font-mono">
+            3 principles, 2 layers to
+            <br />
+            <span className="text-4xl font-semibold text-gray-600">
+              Bring Down 1 Counterfeiting in 2023
+            </span>
+          </h4>
         </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mt-auto grid gap-4 grid-cols-1 lg:grid-cols-3">
         <button
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
