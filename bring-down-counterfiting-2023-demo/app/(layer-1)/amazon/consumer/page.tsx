068ceb6f67984ba9e9f881ad9b313d7fe502ca78
harshit kumar <harshit.kumar@esamudaay.com>
2023-10-30 16:05:21 +0530

    made the consumer section dynamicly load the product listimg

--- bring-down-counterfiting-2023-demo/app/(layer-1)/amazon/consumer/page.tsx
+++ bring-down-counterfiting-2023-demo/app/(layer-1)/amazon/consumer/page.tsx
@@ -1,78 +1,87 @@
 "use client";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { Badge } from "@/components/ui/badge";
+
 import Image from "next/image";
-import { BadgeCheckIcon } from "lucide-react";
+import { BadgeAlert, BadgeCheckIcon } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useEffect, useState } from "react";
+import { ProductListing } from "../product-listings/page";
 
 export default function Consumer() {
+  const [state, setState] = useState({
+    listedProducts: [] as Array<ProductListing>,
+  });
+
+  useEffect(() => {
+    // decode local storage string
+    const localListedProducts: Array<ProductListing> = JSON.parse(
+      localStorage.getItem("listedProducts") ?? "[]"
+    ) as Array<ProductListing>;
+
+    setState((prev) => ({
+      ...prev,
+      listedProducts: localListedProducts,
+    }));
+  }, []);
   return (
-    <ScrollArea className="max-h-[80vh] rounded-md mt-8">
-      <div className="flex flex-col space-y-4 mt-8">
-        <figure
-          className={`md:flex bg-white rounded-xl p-8 md:p-0 dark:bg-slate-800 shadow-md hover:shadow-2xl transition-shadow duration-300 overflow-hidden`}
-        >
-          <div className="flex-none w-48 relative">
-            <Image
-              className="absolute inset-0 w-full h-full object-cover rounded-lg"
-              src="/nike-vapor-vertical-crop.png"
-              alt="Nike Vapor"
-              layout="responsive"
-              objectFit="cover"
-              width={1}
-              height={2}
-            />
-          </div>
-          <div className="flex-auto p-6">
-            <div className="flex flex-wrap">
-              <h1 className="flex-auto text-xl font-semibold">
-                Nike ZoomX Vaporfly NEXT
-              </h1>
-              <BadgeCheckIcon
-                color="orange"
-                width={48}
-                height={48}
-              ></BadgeCheckIcon>
+    <Card className="flex flex-col min-h-[80vh] max-h-[80vh] max-w-[48vw]">
+      <CardHeader className=" lg:mt-2">
+        <CardTitle className="sm:text-lg md:text-2xl lg:text-3xl">
+          Consumer Preview
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        {state.listedProducts.length > 0 && (
+          <ScrollArea className="rounded-md h-[64vh]  border">
+            <div className="flex flex-col-reverse space-y-4 mb-4">
+              {state.listedProducts.map((product, index) => (
+                <figure
+                  key={index}
+                  className={`md:flex mx-8 bg-white rounded-xl p-8 md:p-0 dark:bg-slate-800 shadow-md hover:shadow-2xl transition-shadow duration-300 overflow-hidden`}
+                >
+                  <div className="flex-none w-48 relative">
+                    <Image
+                      className="absolute inset-0 w-full h-full object-cover rounded-lg"
+                      src="/nike-vapor-vertical-crop.png"
+                      alt="Nike Vapor"
+                      layout="responsive"
+                      objectFit="cover"
+                      width={1}
+                      height={2}
+                    />
+                  </div>
+                  <div className="flex-auto p-6">
+                    <div className="flex flex-wrap align-middle items-center">
+                      <h1 className="flex-auto text-xl font-semibold">
+                        Nike ZoomX Vaporfly NEXT
+                      </h1>
+                      {(() => {
+                        const foundDCC = product.dccNodeId !== "";
+                        if (foundDCC && product.status === 1) {
+                          return <BadgeCheckIcon width={48} height={48} />;
+                        }
+                        if (foundDCC && product.status === 0) {
+                          return (
+                            <BadgeAlert color="orange" width={48} height={48} />
+                          );
+                        }
+                      })()}
+                    </div>
+                    <p className="mt-4 text-sm">
+                      The Nike ZoomX Vaporfly NEXT is the fast you&apos;ve never
+                      seen—or felt—before. By combining our two most innovative
+                      technologies, Nike ZoomX foam and VaporWeave material,
+                      it&apos;s the fastest shoe we&apos;ve ever made. Scroll
+                      down to learn more about the future of racing shoes.
+                    </p>
+                  </div>
+                </figure>
+              ))}
             </div>
-            <p className="mt-4 text-sm">
-              The Nike ZoomX Vaporfly NEXT is the fast you&apos;ve never seen—or
-              felt—before. By combining our two most innovative technologies,
-              Nike ZoomX foam and VaporWeave material, it&apos;s the fastest
-              shoe we&apos;ve ever made. Scroll down to learn more about the
-              future of racing shoes.
-            </p>
-          </div>
-        </figure>
-        <figure
-          className={`md:flex bg-white rounded-xl p-8 md:p-0 dark:bg-slate-800 shadow-md hover:shadow-2xl transition-shadow duration-300 overflow-hidden`}
-        >
-          <div className="flex-none w-48 relative">
-            <Image
-              className="absolute inset-0 w-full h-full object-cover rounded-lg"
-              src="/nike-vapor-vertical-crop.png"
-              alt="Nike Vapor"
-              layout="responsive"
-              objectFit="cover"
-              width={1}
-              height={2}
-            />
-          </div>
-          <div className="flex-auto p-6">
-            <div className="flex flex-wrap justify-between">
-              <h1 className="flex-auto text-xl font-semibold col">
-                Nike ZoomX Vaporfly NEXT
-              </h1>
-            </div>
-            <p className="mt-4 text-sm">
-              The Nike ZoomX Vaporfly NEXT is the fast you&apos;ve never seen—or
-              felt—before. By combining our two most innovative technologies,
-              Nike ZoomX foam and VaporWeave material, it&apos;s the fastest
-              shoe we&apos;ve ever made. Scroll down to learn more about the
-              future of racing shoes.
-            </p>
-          </div>
-        </figure>
-      </div>
-      <ScrollBar orientation="vertical" />
-    </ScrollArea>
+            <ScrollBar orientation="vertical" />
+          </ScrollArea>
+        )}
+      </CardContent>
+    </Card>
   );
 }
