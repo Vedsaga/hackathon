a3980eb99114eca2cf06e07270d698bb64b73836
harshit kumar <harshit.kumar@esamudaay.com>
2023-11-02 12:11:37 +0530

    make the icon filled

--- bring-down-counterfiting-2023-demo/app/(layer-1)/amazon/consumer/page.tsx
+++ bring-down-counterfiting-2023-demo/app/(layer-1)/amazon/consumer/page.tsx
@@ -24,7 +24,7 @@ export default function Consumer() {
     }));
   }, []);
   return (
-    <Card className="flex flex-col min-h-[80vh] max-h-[80vh] max-w-[48vw]">
+    <Card className="flex flex-col min-h-[80vh] max-h-[80vh] max-w-[80vw]">
       <CardHeader className=" lg:mt-2">
         <CardTitle className="sm:text-lg md:text-2xl lg:text-3xl">
           Consumer Preview
@@ -32,7 +32,7 @@ export default function Consumer() {
       </CardHeader>
       <CardContent>
         {state.listedProducts.length > 0 && (
-          <ScrollArea className="rounded-md h-[64vh]  border">
+          <ScrollArea className="rounded-md h-[64vh]">
             <div className="flex flex-col-reverse space-y-4 mb-4">
               {state.listedProducts.map((product, index) => (
                 <figure
@@ -58,11 +58,40 @@ export default function Consumer() {
                       {(() => {
                         const foundDCC = product.dccNodeId !== "";
                         if (foundDCC && product.status === 1) {
-                          return <BadgeCheckIcon width={48} height={48} />;
+                          return (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="48"
+                              height="48"
+                              viewBox="0 0 24 24"
+                              fill="#33C361"
+                              stroke="#FFFFFF"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            >
+                              <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+                              <path d="m9 12 2 2 4-4" />
+                            </svg>
+                          );
                         }
                         if (foundDCC && product.status === 0) {
                           return (
-                            <BadgeAlert color="orange" width={48} height={48} />
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="48"
+                              height="48"
+                              viewBox="0 0 24 24"
+                              fill="#FF7F00"
+                              stroke="#FFFFFF"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            >
+                              <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+                              <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+                              <line x1="12" x2="12.01" y1="17" y2="17" />
+                            </svg>
                           );
                         }
                       })()}
